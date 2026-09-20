c5136d55aa153dac5ea5aa2c8a3a9435ee8e1b5f
Graziele <grazieleltorres@gmail.com>
2026-09-20 10:35:36 -0300

    Model e controller do usuário

---
+++ api-web-services-fuel-manager/Migrations/20260919231450_M01.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api_web_services_fuel_manager.Migrations
+{
+    /// <inheritdoc />
+    public partial class M01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LinkDto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Href = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rel = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Metodo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConsumoId = table.Column<int>(type: "int", nullable: true),
+                    VeiculoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LinkDto", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LinkDto_Consumos_ConsumoId",
+                        column: x => x.ConsumoId,
+                        principalTable: "Consumos",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_LinkDto_Veiculos_VeiculoId",
+                        column: x => x.VeiculoId,
+                        principalTable: "Veiculos",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Perfil = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VeiculoUsuarios",
+                columns: table => new
+                {
+                    VeiculoId = table.Column<int>(type: "int", nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VeiculoUsuarios", x => new { x.VeiculoId, x.UsuarioId });
+                    table.ForeignKey(
+                        name: "FK_VeiculoUsuarios_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_VeiculoUsuarios_Veiculos_VeiculoId",
+                        column: x => x.VeiculoId,
+                        principalTable: "Veiculos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LinkDto_ConsumoId",
+                table: "LinkDto",
+                column: "ConsumoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LinkDto_VeiculoId",
+                table: "LinkDto",
+                column: "VeiculoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VeiculoUsuarios_UsuarioId",
+                table: "VeiculoUsuarios",
+                column: "UsuarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LinkDto");
+
+            migrationBuilder.DropTable(
+                name: "VeiculoUsuarios");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
